cde2313f79967485d8d4210b4d27b8cd6b96a626
Ragib Badaruddin <ragib@seekasia.com>
2020-11-29 07:23:55 +0800

    fix: Minor type fix in callback

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,19 @@
 type Callback = (err: any, data: any) => void;
 type AnyFunc = (...args: any[]) => void;
 
+type Data<F extends Callback> = F extends (err: any, data: infer T) => void ? T : never;
+type Err<F extends Callback> = F extends (err: infer T, data: any) => void ? T : never;
+
 type ArgsOf<F extends AnyFunc> = Parameters<F> extends [...args: infer I, cb?: Callback]
   ? I
   : never;
 
-type CallbackOf<F extends AnyFunc> = Parameters<F> extends [
-  ...args: infer _I,
-  cb?: infer C & Callback,
-]
+type CallbackOf<F extends AnyFunc> = Parameters<F> extends [...args: infer _I, cb?: infer C]
   ? C
   : never;
 
-type ReturnOf<F extends AnyFunc> = CallbackOf<F> extends Callback &
-  ((err: any, data: infer T) => void)
-  ? T
-  : never;
-
-type ErrorOf<F extends AnyFunc> = CallbackOf<F> extends Callback &
-  ((err: infer E, data: any) => void)
-  ? E
-  : never;
+type ReturnOf<F extends AnyFunc> = CallbackOf<F> extends Callback ? Data<CallbackOf<F>> : never;
+type ErrorOf<F extends AnyFunc> = CallbackOf<F> extends Callback ? Err<CallbackOf<F>> : never;
 
 export default function promisify<F extends AnyFunc>(fn: F) {
   return function (...args: ArgsOf<F>): Promise<ReturnOf<F>> {
